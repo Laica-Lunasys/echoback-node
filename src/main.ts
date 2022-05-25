500494e089d7491d1d320f0ff08a576196fa0a48
Laica Lunasys <laica.lunasys@gmail.com>
2022-05-25 12:10:03 +0900

    Add actions

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logtest } from './logtest';
 
 async function bootstrap() {
+  logtest();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
